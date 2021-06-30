d0536fef7467158940f7435f385041c7f828c4b8
Christian castro <Christian castro@192.168.0.17>
2021-06-30 19:38:23 +0100

    pepe 2

--- src/main/java/Clases/Empleados.java
+++ src/main/java/Clases/Empleados.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor. gf
+ * and open the template in i the editor. gf
  */
 package Clases;
 
